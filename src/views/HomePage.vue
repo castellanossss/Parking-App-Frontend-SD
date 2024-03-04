d9dfaa65cda3cab9e57a8eb155d5eb8aed367edd
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2024-03-04 01:11:30 -0500

    Push 01

---
+++ src/views/HomePage.vue
@@ -0,0 +1,519 @@
+<template>
+    <div>
+        <div v-if="serverStatusMessage" class="alert alert-danger">
+            {{ serverStatusMessage }}
+        </div>
+
+        <div v-if="!showCarsList && !showRegistrationForm && !showWithdrawForm" class="welcome-menu">
+            <h2>Welcome to the Parking System</h2>
+            <div class="menu-options">
+                <button @click="showRegisterForm">Register Car</button>
+                <button @click="listCars">List Cars</button>
+                <button @click="showWithdrawCarForm">Withdraw Car</button>
+            </div>
+
+            <div v-if="withdrawAlertMessageA" class="alert alert-info mt-3">
+                {{ withdrawAlertMessageA }}
+            </div>
+        </div>
+
+        <div v-if="showRegistrationForm" class="withdraw-form">
+            <h2>Register a Car</h2>
+            <form @submit.prevent="registerCar">
+                <input type="text" v-model="car.licensePlate" placeholder="License Plate" required>
+                <div class="form-group">
+                    <label for="carColor">Color:</label>
+                    <input type="color" id="carColor" v-model="car.color" required>
+                </div>
+                <input type="file" @change="handleFileUpload" required>
+                <div class="button-group">
+                    <button type="submit">Submit</button>
+                    <button type="button" @click="showMenu">Back to Menu</button>
+                </div>
+            </form>
+
+            <div v-if="alertMessage" class="alert" :class="{ 'alert-success': success, 'alert-danger': !success }">
+                {{ alertMessage }}
+            </div>
+        </div>
+
+        <div v-if="showCarsList" class="car-list-container">
+            <h2 class="car-list-title">List of Registered Cars</h2>
+            <div v-for="car in cars" :key="car.licensePlate" class="car-item">
+                <img :src="`http://${this.load_balancer_url}/uploads/${car.imagePath}`" alt="Car photo"
+                    class="car-image" />
+                <div class="car-details">
+                    <p><strong>Plate:</strong> {{ car.licensePlate }}</p>
+                    <p><strong>Color:</strong> {{ car.color }}</p>
+                    <p><strong>Entry Time:</strong> {{ new Date(car.timestamp).toLocaleString() }}</p>
+                </div>
+            </div>
+            <button class="back-to-menu-btn" @click="showMenuB">Back to Menu</button>
+        </div>
+
+        <div v-if="noCarsRegistered && showCarsList" class="alert alert-info">
+            <p>No cars have been registered yet.</p>
+        </div>
+
+        <div v-if="showWithdrawForm" class="registration-form">
+            <h2>Withdraw a Car</h2>
+            <form @submit.prevent="withdrawCar">
+                <input type="text" v-model="withdrawLicensePlate" placeholder="License Plate" required>
+                <div class="button-group">
+                    <button type="submit">Withdraw</button>
+                    <button type="button" @click="showMenuC">Back to Menu</button>
+                </div>
+            </form>
+
+            <div v-if="withdrawAlertMessageB" class="alert"
+                :class="{ 'alert-success': success, 'alert-danger': !success }">
+                {{ withdrawAlertMessageB }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            load_balancer_url: process.env.VUE_APP_LOAD_BALANCER_URL_SD,
+            serverOnline: true,
+            errorMessage: '',
+            successMessage: '',
+            selectedFileName: '',
+            car: {
+                licensePlate: '',
+                color: '',
+                photo: null,
+                entryTime: ''
+            },
+            showRegistrationForm: false,
+            alertMessage: '',
+            success: true,
+            cars: [],
+            showCarsList: false,
+            noCarsRegistered: false,
+            withdrawLicensePlate: '',
+            showWithdrawForm: false,
+            withdrawAlertMessageA: '',
+            withdrawAlertMessageB: '',
+            serverStatusMessage: '',
+        };
+    },
+    methods: {
+        updateServerStatus(online, message) {
+            this.serverOnline = online;
+            this.serverStatusMessage = message;
+        },
+
+        checkServerStatus() {
+            fetch(`http://${this.load_balancer_url}/cars/test-connection`)
+                .then(response => {
+                    if (!response.ok) throw new Error('Server is not responding');
+                    this.updateServerStatus(true, '');
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.updateServerStatus(false, 'Cannot connect to the server. Please try again later.');
+                });
+        },
+
+        registerCar() {
+            this.alertMessage = '';
+            this.selectedFileName = '';
+
+            const formData = new FormData();
+            formData.append('license_plate', this.car.licensePlate);
+            formData.append('color', this.car.color);
+            formData.append('photo', this.car.photo);
+            formData.append('entryTime', new Date().toISOString());
+
+            fetch(`http://${this.load_balancer_url}/cars/register`, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    if (!response.ok) throw new Error('Failed to register the car, verify the data entered.');
+                    return response.json();
+                })
+                // eslint-disable-next-line no-unused-vars
+                .then(data => {
+                    this.alertMessage = 'Car registered successfully!';
+                    this.success = true;
+                    this.car.licensePlate = '';
+                    this.car.color = '';
+                    this.car.photo = null;
+
+                    setTimeout(() => {
+                        this.alertMessage = '';
+                        this.showRegistrationForm = false;
+                    }, 2000);
+                })
+                .catch(error => {
+                    this.alertMessage = error.message;
+                    this.success = false;
+                });
+        },
+
+        showMenu() {
+            this.car.licensePlate = '';
+            this.car.color = '';
+            this.car.photo = null;
+            this.showRegistrationForm = false;
+        },
+
+        handleFileUpload(event) {
+            const files = event.target.files;
+
+            if (files.length > 0) {
+                const file = files[0];
+                this.car.photo = file;
+                this.selectedFileName = file.name;
+            } else {
+                this.car.photo = null;
+                this.selectedFileName = '';
+            }
+        },
+
+        showRegisterForm() {
+            this.showRegistrationForm = true;
+        },
+
+        listCars() {
+            this.showCarsList = true;
+            this.noCarsRegistered = false;
+            this.checkServerStatus();
+
+            if (!this.serverOnline) {
+                return;
+            }
+
+            fetch(`http://${this.load_balancer_url}/cars/list`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to load cars.');
+                    }
+                    return response.json();
+                })
+                .then(carsFromServer => {
+                    this.cars = carsFromServer;
+                    this.serverOnline = true;
+                    if (this.cars.length === 0) {
+                        this.noCarsRegistered = true;
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.serverOnline = false;
+                    this.cars = [];
+                    this.noCarsRegistered = true;
+                });
+        },
+
+        showMenuB() {
+            this.showCarsList = false;
+            this.noCarsRegistered = false;
+        },
+
+        withdrawCar() {
+            fetch(`http://${this.load_balancer_url}/cars/withdraw`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ license_plate: this.withdrawLicensePlate })
+            })
+                .then(response => {
+                    if (!response.ok) throw new Error('Error withdrawing car.');
+                    return response.json();
+                })
+                .then(data => {
+                    this.withdrawAlertMessageB = data.message;
+                    this.success = true;
+                    setTimeout(() => {
+                        this.showWithdrawForm = false;
+                        this.withdrawLicensePlate = '';
+                        this.withdrawAlertMessageB = '';
+                    }, 3000);
+                })
+                .catch(error => {
+                    this.withdrawAlertMessageB = error.message;
+                    this.success = false;
+                });
+        },
+
+        showWithdrawCarForm() {
+            if (this.cars.length === 0) {
+                this.withdrawAlertMessageA = 'No cars to withdraw.';
+
+                setTimeout(() => {
+                    this.withdrawAlertMessageA = '';
+                }, 5000);
+
+                this.success = false;
+                this.showWithdrawForm = false;
+                this.showRegistrationForm = false;
+                this.showCarsList = false;
+            } else {
+                this.withdrawAlertMessageA = '';
+                this.showWithdrawForm = true;
+                this.showRegistrationForm = false;
+                this.showCarsList = false;
+                this.noCarsRegistered = false;
+            }
+        },
+
+        showMenuC() {
+            this.showWithdrawForm = false;
+        },
+    },
+
+    mounted() {
+        this.checkServerStatus();
+    }
+};
+</script>
+
+<style>
+body,
+html {
+    height: auto;
+    min-height: 100%;
+    overflow-y: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Arial', sans-serif;
+    background-color: #f7f7f7;
+}
+
+.welcome-menu {
+    text-align: center;
+    width: 100%;
+    padding: 50px;
+}
+
+.welcome-menu h2 {
+    margin-bottom: 20px;
+}
+
+.menu-options button {
+    margin: 10px;
+    padding: 10px 20px;
+    font-size: 1rem;
+    cursor: pointer;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+}
+
+.menu-options button:hover {
+    background-color: #0056b3;
+}
+
+.registration-form {
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    padding: 2rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    background-color: white;
+    border-radius: 8px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.registration-form h2 {
+    margin-bottom: 1rem;
+}
+
+.form-group {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+}
+
+.form-group label {
+    margin-right: 0.5rem;
+}
+
+input[type="color"] {
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    width: 2.5em;
+    height: 2.5em;
+    cursor: pointer;
+    background: transparent;
+    outline: none;
+    padding: 0;
+}
+
+input[type="color"]::-webkit-color-swatch-wrapper {
+    padding: 0;
+    border: none;
+}
+
+input[type="color"]::-webkit-color-swatch {
+    border: none;
+}
+
+input[type="color"]::-moz-color-swatch {
+    border: none;
+}
+
+input[type="color"]::-moz-focus-inner {
+    border: 0;
+}
+
+.registration-form input,
+.registration-form button {
+    padding: 0.75rem 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+}
+
+.registration-form button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.2s;
+}
+
+.registration-form button:not(:last-child) {
+    margin-right: 0.5rem;
+}
+
+.registration-form button:hover {
+    background-color: #0056b3;
+}
+
+.custom-file-input {
+    display: inline-block;
+}
+
+.button-group {
+    display: flex;
+    justify-content: space-between;
+}
+
+.alert {
+    text-align: center;
+    padding: 10px;
+    margin-top: 20px;
+    border-radius: 4px;
+}
+
+.alert-success {
+    background-color: #d4edda;
+    color: #155724;
+}
+
+.alert-danger {
+    background-color: #f8d7da;
+    color: #721c24;
+}
+
+.car-list-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(450px, 550px));
+    grid-gap: 20px;
+    margin-top: 20px;
+    padding: 20px;
+}
+
+.car-list-title {
+    text-align: center;
+    margin-top: 0;
+    padding-top: 20px;
+}
+
+.car-item {
+    border: 1px solid #ccc;
+    padding: 16px;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+}
+
+.car-item img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 8px;
+}
+
+.car-details {
+    padding: 0.5rem;
+}
+
+.car-details p {
+    margin: 0.5rem 0;
+}
+
+.back-to-menu-btn {
+    display: block;
+    width: auto;
+    margin: 20px auto;
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.back-to-menu-btn:hover {
+    background-color: #0056b3;
+}
+
+.withdraw-form {
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    padding: 2rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    background-color: white;
+    border-radius: 8px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.withdraw-form h2 {
+    margin-bottom: 1rem;
+}
+
+.withdraw-form input,
+.withdraw-form button {
+    padding: 0.75rem 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+}
+
+.withdraw-form button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.2s;
+}
+
+.withdraw-form button:not(:last-child) {
+    margin-right: 0.5rem;
+}
+
+.withdraw-form button:hover {
+    background-color: #0056b3;
+}
+</style>
