217a38daf2755f85b869627c174acae3a5094228
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2024-03-11 11:24:52 -0500

    Modified generate-config.js

--- generate-config.js
+++ generate-config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const config = {
-    loadBalancerUrl: process.env.LOAD_BALANCER_URL
+    load_balancer_url: process.env.LOAD_BALANCER_URL
 };
 
 fs.writeFileSync('public/config.js', `window.config = ${JSON.stringify(config)};`);
